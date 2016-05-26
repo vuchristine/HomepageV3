9dfc00236a9a202a85298a8367aea725d05985a7
Christine <vuchris@uw.edu>
2016-05-26 14:24:42 -0700

    I have tried to invite a friend by adding code from http://code.tutsplus.com/tutorials/how-to-grow-your-audience-with-google-app-invites--cms-25321. So far it's not working. I'm hoping to commit this to the branch instead of the master.

---
+++ app/src/main/java/vuchris/tacoma/uw/edu/homepagev3/InviteDetailsActivity.java
@@ -0,0 +1,33 @@
+package vuchris.tacoma.uw.edu.homepagev3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.appinvite.AppInviteReferral;
+
+public class InviteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_details);
+
+        Intent intent = getIntent();
+        if (AppInviteReferral.hasReferral(intent)) {
+            // Extract the information from the Intent
+            String deepLink = AppInviteReferral.getDeepLink(intent);
+            Log.d(getString(R.string.app_name),
+                    "Found Referral: " + AppInviteReferral.getInvitationId(intent) + ":" + deepLink);
+
+            String[] array = deepLink.split("/");
+
+            if (array.length > 0) {
+                TextView tv = (TextView) findViewById(R.id.discount);
+                tv.setText(String.format(tv.getText().toString(), array[array.length-1]));
+            }
+        }
+    }
+}
